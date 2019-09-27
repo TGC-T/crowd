4029e6fd64edc08bffb2028463a9e0655faa4c93
Arthur Khakimov <verybigfolder@gmail.com>
2019-09-28 01:49:41 +0300

    Add bootstrap

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from json import dumps as json
+from datetime import datetime
 app = Flask(__name__)
 
 
@@ -18,20 +19,24 @@ def checkInput(FirstName, LastName, Email):
                 break
     else:
         IncorrectInput.append('LastName')
-
     if not '@' in Email:
         IncorrectInput.append('Email')
-
     if (len(IncorrectInput) == 0):
         return True
     return IncorrectInput
 
-def addcrowdposttodb(name: str, description: str, amounttoget: float, urgency: int):
+# def getCurrentid():
+#     client = MongoClient('localhost', 27017)
+#     db = client.posts
+#     collection = db.indexes
+
+
+def addcrowdposttodb(name: str, description: str, amounttoget: float):
     '''
     Добавляет краудфанд в базу с нулевым балансом
     '''
     post = {'name': name, 'description': description, 'amounttoget': amounttoget,
-            'wegot': 0, 'urgency': urgency, 'iscomplete': False}
+            'wegot': 0, 'iscomplete': False}
     from pymongo import MongoClient
     client = MongoClient('localhost', 27017)
     db = client.posts
@@ -108,12 +113,11 @@ def userLogin():
 
 @app.route('/api/crowd/add')
 def addCrowd():
-    #post = {'name':name, 'description': description, 'amounttoget': amounttoget, 'wegot':0, 'urgency':urgency, 'iscomplete':False}
+    #post = {'name':name, 'description': description, 'amounttoget': amounttoget, 'wegot':0, 'iscomplete':False, my_id: 4}
     name = request.args.get('name')
     description = request.args.get('description')
     amounttoget = request.args.get('amounttoget')
-    urgency = request.args.get('amounttoget')
-    addcrowdposttodb(name, description, int(amounttoget), int(urgency))
+    addcrowdposttodb(name, description, int(amounttoget))
     return json({"Result": True, "What": None})
 
 
@@ -151,9 +155,62 @@ def getall():
     result = []
     for i in collection.find({}):
         post = {'name': i['name'], 'description': i['description'],
-                'amounttoget': i['amounttoget'], 'wegot': i['wegot'], 'urgency': i['urgency']}
+                'amounttoget': i['amounttoget'], 'wegot': i['wegot']}
         result.append(post)
-    return json(result)
+    return 
+def getTop3Crowd():
+    from pymongo import MongoClient
+    client = MongoClient('localhost', 27017)
+    db = client.posts
+    collection = db.tasks
+    posts = []
+    count = 2
+    for i in collection.find({}):
+        posts.append({'name': i['name'], 'description': i['description'], '_id': i['_id']})
+        if count == 0:
+            break
+        count -= 1
+    return posts
+
+
+@app.route('/')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Домашняя страница',
+        year=datetime.now().year,
+        posts = getTop3Crowd()
+    )
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Контакты',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='О нас',
+        year=datetime.now().year,
+        message='Описание страницы'
+    )
+
+@app.route('/crowd/<id_str>')
+def showCrowd(id_str):
+    from bson.objectid import ObjectId
+    from pymongo import MongoClient
+    client = MongoClient('localhost', 27017)
+    db = client.posts
+    collection = db.tasks
+    finded = collection.find_one({"_id": ObjectId(id_str)})
+    return render_template('crowd.html',title = finded['name'], name=finded['name'], description=finded['description'], need=finded['amounttoget'], wegot=finded['wegot'], persent = int(finded['wegot']/finded['amounttoget'] * 100))
 
 
 app.run(debug=True, host="0.0.0.0")
