6838530999fed4154a644d8d7211ef1a6c2ab9f4
Arthur Khakimov <verybigfolder@gmail.com>
2019-09-27 19:01:30 +0300

    Add setter for crowds

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from json import dumps as json
 app = Flask(__name__)
 
 
-
-def addcrowdposttodb(name:str, description:str, amounttoget:float, urgency:int):
+def addcrowdposttodb(name: str, description: str, amounttoget: float, urgency: int):
     '''
     Добавляет краудфанд в базу с нулевым балансом
     '''
-    post = {'name':name, 'description': description, 'amounttoget': amounttoget, 'wegot':0, 'urgency':urgency, 'iscomplete':False}
+    post = {'name': name, 'description': description, 'amounttoget': amounttoget,
+            'wegot': 0, 'urgency': urgency, 'iscomplete': False}
     from pymongo import MongoClient
     client = MongoClient('localhost', 27017)
     db = client.posts
@@ -18,21 +18,12 @@ def addcrowdposttodb(name:str, description:str, amounttoget:float, urgency:int):
     post_id = collection.insert_one(post)
     return post_id
 
-def modcrowdpost(post_id, update_money):
-    '''
-    Изменяет кол-во полученных сейчас денег
-    '''
-    from pymongo import MongoClient
-    client = MongoClient('localhost', 27017)
-    db = client.posts
-    collection = db.tasks
-    collection.update_one(post_id, {'wegot':update_money})
 
-def registerUser(email:str, pwhash:str, fio:str):
+def registerUser(email: str, pwhash: str, fio: str):
     '''
     регистрация пользователя
     '''
-    post = {'email': email, 'pwhash': pwhash, 'fio' : fio}
+    post = {'email': email, 'pwhash': pwhash, 'fio': fio}
     from pymongo import MongoClient
     client = MongoClient('localhost', 27017)
     db = client.webusers
@@ -40,7 +31,8 @@ def registerUser(email:str, pwhash:str, fio:str):
     post_id = collection.insert_one(post)
     return post_id
 
-def checkUser(email:str, pwhash:str):
+
+def checkUser(email: str, pwhash: str):
     '''
     проверка сущ пользьзователя
     '''
@@ -51,12 +43,14 @@ def checkUser(email:str, pwhash:str):
     db = client.webusers
     collection = db.users
     finded = collection.find_one(post)
-    if  finded is None:
+    if finded is None:
         return False
     else:
         if finded['pwhash'] != pwhash:
             return False
         return True
+
+
 @app.route('/api/user/register')
 def userRegister():
     # url /user/register?email=example@example.com&pwhash=AAAAAAAAAA&fio=ArthurKhakimovMarathovich
@@ -66,8 +60,9 @@ def userRegister():
     try:
         registerUser(email, pwhash, fio)
     except Exception:
-        return json({'Result' : False, 'What' : 'Пользователь уже существуют'})
-    return json({'Result' : True, 'What' : None})
+        return json({'Result': False, 'What': 'Пользователь уже существуют'})
+    return json({'Result': True, 'What': None})
+
 
 @app.route('/api/user/login')
 def userLogin():
@@ -75,8 +70,9 @@ def userLogin():
     email = request.args.get('email')
     pwhash = request.args.get('pwhash')
     if checkUser(email, pwhash):
-        return json({'Result' : True, 'What': None})
-    return json({'Result' : False, 'What':'Неверный пароль'})
+        return json({'Result': True, 'What': None})
+    return json({'Result': False, 'What': 'Неверный пароль'})
+
 
 @app.route('/api/crowd/add')
 def addCrowd():
@@ -86,17 +82,27 @@ def addCrowd():
     amounttoget = request.args.get('amounttoget')
     urgency = request.args.get('amounttoget')
     addcrowdposttodb(name, description, int(amounttoget), int(urgency))
-    return json({"Result" : True, "What" : None})
+    return json({"Result": True, "What": None})
 
-@app.route('/api/crowd/modify')
-def modCrowd():
+
+@app.route('/api/crowd/modify/<field>')
+def modCrowd(field):
+    # /api/crowd/modify/name?old=Test&new=Test1
     from pymongo import MongoClient
     client = MongoClient('localhost', 27017)
     db = client.posts
     collection = db.tasks
-    name = request.args.get('name')
-    post_id = collection.find_one({'name':name})
-    modcrowdpost(post_id, int(request.args.get('amounttoget')))
-    return json({"Result" : True, "What" : None})
+    find = {field: request.args.get('old')}
+    newvalue = {"$set": {field: request.args.get('new')}}
+    collection.update_one(find, newvalue)
+    return json({"Result": True, "What": None})
+
+# @app.route('/api/crowd/get/<field>')
+# def getCrowd(field):
+#     from pymongo import MongoClient
+#     client = MongoClient('localhost', 27017)
+#     db = client.posts
+#     collection = db.tasks
+
 
 app.run(debug=True, host="0.0.0.0")
